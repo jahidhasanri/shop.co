31cd1e1220e6d73be4b1dab77105699a2bb4123e
jahid hasan rifat <jahadhasanrifat@gmail.com>
2025-06-16 00:24:50 +0600

    add brands component

---
+++ src/Components/Brands.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Brands = () => {
+    return (
+        <div className='bg-black'>
+            <div className='container mx-auto lg:flex gap-2 items-center justify-between'>
+                <img className='mb-6 lg:mt-[44px] lg:mb-[44px]' src="https://i.ibb.co/k2ZsMtzG/Vector.png" alt="" />
+                <img className='mb-6 lg:mb-0' src="https://i.ibb.co/bR5JCXkF/zara-logo-1-1.png" alt="" />
+                <img className='mb-6 lg:mb-0' src="https://i.ibb.co/RTwF75c1/gucci-logo-1-1.png" alt="" />
+                <img className='mb-6 lg:mb-0' src="https://i.ibb.co/GQVJFG3F/prada-logo-1-1.png" alt="" />
+                <img className='' src="https://i.ibb.co/rK7TXHyM/Group.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Brands;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Components/Banner';
+import Brands from '../Components/Brands';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Brands></Brands>
         </div>
     );
 };
